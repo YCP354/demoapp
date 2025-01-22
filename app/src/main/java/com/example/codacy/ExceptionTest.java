6b12475dc161fef61324a3a45bf6d3bc6866c481
杨昌平 <yangchangping@yolanda.hk>
2025-01-22 16:03:16 +0800

    update(main):测试codyacy检测

---
+++ app/src/main/java/com/example/codacy/ExceptionTest.java
@@ -0,0 +1,8 @@
+package com.example.codacy;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        String text = null;
+        System.out.println(text.length());  // 空指针异常
+    }
+}
