efa6c179a5de9efee793ff59a683415740956a30
杨昌平 <yangchangping@yolanda.hk>
2025-07-21 11:00:02 +0800

    update(f-compose):完成示例

---
+++ app/src/main/java/com/example/compose/bean/Message.kt
@@ -0,0 +1,8 @@
+package com.example.compose.bean
+
+/*
+ * Copyright (c) 2025 ycp
+ * All rights reserved.
+ *
+ * Created by ycp on 2025/7/21.
+ */data class Message(val author: String, val body: String)
