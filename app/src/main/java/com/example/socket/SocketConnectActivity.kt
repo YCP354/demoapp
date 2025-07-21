325ed0c15f8c14fa02cb7ecc31ef01ba4cbd9f21
杨昌平 <yangchangping@yolanda.hk>
2025-07-21 09:26:36 +0800

    update(main):增加快速输入的内容

--- app/src/main/java/com/example/socket/SocketConnectActivity.kt
+++ app/src/main/java/com/example/socket/SocketConnectActivity.kt
@@ -72,21 +72,21 @@ class SocketConnectActivity : AppCompatActivity() {
                 Log.d(TAG, "onClientDisconnected() called with: conn = $conn")            }
         })
         server.start()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // Android 11 及以上
-            window.setDecorFitsSystemWindows(false)
-            val controller = window.insetsController
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.statusBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            // Android 10 及以下
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        }
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            // Android 11 及以上
+//            window.setDecorFitsSystemWindows(false)
+//            val controller = window.insetsController
+//            if (controller != null) {
+//                controller.hide(WindowInsets.Type.statusBars())
+//                controller.systemBarsBehavior =
+//                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//            }
+//        } else {
+//            // Android 10 及以下
+//            window.decorView.systemUiVisibility =
+//                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        }
     }
 
     fun getLocalIpAddress(): String? {
